87cb71bf3b061e3be03b4988d51cf19f8cade234
Eimy Garcia <eimybarrera2019@gmail.com>
2023-09-30 13:26:10 -0500

    Este commit tiene la carpeta formularios

---
+++ The_social_network_camilo_jefernne_eimy/Formularios/login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_social_network_camilo_jefernne_eimy.Formularios
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+    }
+}
